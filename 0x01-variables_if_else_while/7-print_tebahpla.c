9e1a5df9774af1b3120a9f6f91aa6df65c4c3452
Levashan <Levashan97@gmail.com>
2022-09-10 09:53:16 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Alphabet in lowercase
+ *
+ * Description: prints the alphabet in lowercase without q and e
+ *
+ * Return: Succesfull
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
