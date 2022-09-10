9b18033cdee50992ccc54c58e95532b817579d09
Levashan <Levashan97@gmail.com>
2022-09-10 10:14:59 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Possible combos
+ *
+ * Description: possible combos of a single digit
+ *
+ * Return: Successul
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
