5349944e9688e33a22912e35fbacc29eb1b92ea6
Levashan <Levashan97@gmail.com>
2022-09-13 14:58:17 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * @i: absolute value
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+
+	return (i);
+}
