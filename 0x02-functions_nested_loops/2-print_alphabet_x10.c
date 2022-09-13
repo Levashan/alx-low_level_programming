c011f783a408d401e0e7191fddf033634d9f6149
Levashan <Levashan97@gmail.com>
2022-09-13 14:06:57 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Write code 10 times
+ *
+ * Description: alphabet in 10 line
+ *
+ * Return: Successfull
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char a = 'a';
+
+	while (i <= 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		i++;
+		_putchar('\n');
+		a = 'a';
+	}
+}
