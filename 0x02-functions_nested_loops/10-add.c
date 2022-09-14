aa9d5c62e8e77f4aef177a4fa4cde2389d3ee22c
Levashan <Levashan97@gmail.com>
2022-09-14 13:25:48 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - Sum of 2 numbers
+ *
+ * @i: first number
+ *
+ * @x: second number
+ *
+ * Description: returns the sum of 2 numbers
+ *
+ * Return: Successfull
+ */
+
+int add(int i, int x)
+{
+	int sum;
+
+	sum = i + x;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,5 @@ int _abs(int i);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int i, int x);
 void print_to_98(int n);
