f75c1cd85bd3513da242a64659fe092c6b715023
Levashan <Levashan97@gmail.com>
2022-09-15 14:51:56 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else if ((i % 5) == 0)
 		{
-			if (i < 100)
+			if (i != 100)
 			{
 				printf("Buzz ");
 			}
@@ -39,7 +39,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
