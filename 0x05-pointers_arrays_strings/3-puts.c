f669cb236ddd9f72c1900546098186a096576d72
Levashan <Levashan97@gmail.com>
2022-09-19 13:37:26 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string input 
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	char a;
+
+	while (str[i])
+	{
+		a = str[i];
+		putchar(a);
+		i++;
+	}
+	puchar('\n');
+}
