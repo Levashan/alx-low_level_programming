77875333cb855a97a849150c7adc181f1e03af51
Levashan <Levashan97@gmail.com>
2022-09-19 14:36:17 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse string
+ *
+ * @s: input
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int x;
+	char c;
+
+	while (s[i])
+	{
+		putchar(s[i]);
+		i++;
+	}
+	x = i;
+	for (i >= 0; i < x / 2; ++i)
+	{
+		c = s[i];
+		s[i] = s[x - 1 - i];
+		s[x - 1 - i] = c; 
+	}
+}
