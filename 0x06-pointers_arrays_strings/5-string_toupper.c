b6f38c0b0e10f4dc6910d82887b25cc4b1110995
Levashan <Levashan97@gmail.com>
2022-09-22 12:52:51 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Uppercase
+ *
+ * @c: input
+ *
+ * Return: char.
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
