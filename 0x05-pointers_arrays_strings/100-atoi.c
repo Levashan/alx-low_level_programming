d266675de34963895e8cc3d9990e3ea3c6532f88
Levashan <Levashan97@gmail.com>
2022-09-22 13:43:13 -0700

    3

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - String to int
+ *
+ * @s: input;
+ *
+ * Return: int.
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int x;
+	int c;
+
+	while (s[i] != '\0')
+	{
+		x = s[i];
+		if (x < 0)
+		{	
+		}
+	}
+	
+}
