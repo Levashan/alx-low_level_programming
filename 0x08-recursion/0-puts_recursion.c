313b7f8013820f564128fbe7d0ac88378e01e98f
Levashan <Levashan97@gmail.com>
2022-09-28 20:10:04 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while(s[i])
+	if (s[i] == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(s[i]); 
+	s += 1;
+	_puts_recursion(s);
 }
